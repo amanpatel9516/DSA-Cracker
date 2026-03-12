6c4aac4007f6baba8b82b1b62fcc82a96e4ed85a
Aman Patel <148473527+amanpatel9516@users.noreply.github.com>
2026-03-12 17:00:41 +0530

    Time: 65 ms (79.17%), Space: 268.5 MB (33.33%) - LeetHub

---
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+class Solution {
+
+    class DSU {
+        int[] parent, rank;
+
+        DSU(int n){
+            parent = new int[n];
+            rank = new int[n];
+            for(int i=0;i<n;i++) parent[i] = i;
+        }
+
+        int find(int x){
+            if(parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        boolean union(int a, int b){
+            int pa = find(a);
+            int pb = find(b);
+
+            if(pa == pb) return false;
+
+            if(rank[pa] < rank[pb])
+                parent[pa] = pb;
+            else if(rank[pb] < rank[pa])
+                parent[pb] = pa;
+            else{
+                parent[pb] = pa;
+                rank[pa]++;
+            }
+
+            return true;
+        }
+    }
+
+    public int maxStability(int n, int[][] edges, int k) {
+
+        int left = 1, right = 200000, ans = -1;
+
+        while(left <= right){
+
+            int mid = left + (right-left)/2;
+
+            if(canBuild(n, edges, k, mid)){
+                ans = mid;
+                left = mid + 1;
+            }
+            else
+                right = mid - 1;
+        }
+
+        return ans;
+    }
+
+    boolean canBuild(int n, int[][] edges, int k, int X){
+
+        DSU dsu = new DSU(n);
+
+        int used = 0;
+        int upgrades = 0;
+
+        // mandatory edges
+        for(int[] e : edges){
+            int u=e[0], v=e[1], s=e[2], must=e[3];
+
+            if(must==1){
+                if(s < X) return false;
+
+                if(!dsu.union(u,v))
+                    return false;
+
+                used++;
+            }
+        }
+
+        // optional edges without upgrade
+        for(int[] e : edges){
+            int u=e[0], v=e[1], s=e[2], must=e[3];
+
+            if(must==0 && s>=X){
+                if(dsu.union(u,v))
+                    used++;
+            }
+        }
+
+        // optional edges with upgrade
+        for(int[] e : edges){
+            int u=e[0], v=e[1], s=e[2], must=e[3];
+
+            if(must==0 && s<X && s*2>=X && upgrades<k){
+                if(dsu.union(u,v)){
+                    used++;
+                    upgrades++;
+                }
+            }
+        }
+
+        return used == n-1;
+    }
+}

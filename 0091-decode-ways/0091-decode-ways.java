f3816efa21d22f0578810bf0e380c91d6cf5d6a3
Aman Patel <148473527+amanpatel9516@users.noreply.github.com>
2026-07-23 11:33:44 +0530

    Time: 0 ms (100%), Space: 43 MB (76.5%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,34 +1,31 @@
 class Solution {
+
     public int numDecodings(String s) {
-        int n = s.length();
-        if (n == 1 || s.charAt(0) == '0') {
-            return s.charAt(0) >= '1' && s.charAt(0) <= '9' ? 1 : 0;
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return solve(0, s, dp);
+    }
+
+    private int solve(int p, String s, int[] dp) {
+
+        if (p == s.length())
+            return 1;
+
+        if (s.charAt(p) == '0')
+            return 0;
+
+        if (dp[p] != -1)
+            return dp[p];
+
+        int ans = solve(p + 1, s, dp);
+
+        if (p < s.length() - 1 &&
+            (s.charAt(p) == '1' ||
+            (s.charAt(p) == '2' && s.charAt(p + 1) <= '6'))) {
+
+            ans += solve(p + 2, s, dp);
         }
 
-        int[] dp = new int[n];
-
-        int d1 = s.charAt(0) - '0';
-        int d2 = s.charAt(1) - '0';
-
-        dp[0] = d1 >= 1 && d1 <= 9 ? 1 : 0;
-
-        dp[1] += d2 >= 1 && d2 <= 9 ? 1 : 0;
-        dp[1] += d1 * 10 + d2 >= 1 && d1 * 10 + d2 <= 26 && d1 != 0 ? 1 : 0;
-
-        for (int i = 2; i < n; i++) {
-            d1 = s.charAt(i - 1) - '0';
-            d2 = s.charAt(i) - '0';
-
-            int num = d1 * 10 + d2;
-
-            int sum = (d2 >= 1 && d2 <= 9 ? dp[i - 1] : 0);
-            sum += (num >= 1 && num <= 26 && d1 != 0 ? dp[i - 2] : 0);
-
-            dp[i] = sum;
-
-            if (dp[i] == 0) return 0;
-        }
-
-        return dp[n - 1];
+        return dp[p] = ans;
     }
 }
